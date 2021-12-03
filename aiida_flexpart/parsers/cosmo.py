39741251b1f0b4112532beab149983c2c6cfdfb1
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2021-12-03 17:32:41 +0000

    Fix parser.

--- aiida_flexpart/parsers/cosmo.py
+++ aiida_flexpart/parsers/cosmo.py
@@ -10,7 +10,7 @@ from aiida.plugins import CalculationFactory
 from aiida.common import exceptions
 from aiida.orm import SinglefileData
 
-FlexpartCalculation = CalculationFactory('flexpart')
+FlexpartCalculation = CalculationFactory('flexpart.cosmo')
 
 
 class FlexpartCosmoParser(Parser):
