9028726c6e26ca4a3df1b41471176ffd78b43cd4
LucR31 <lucas.fernandez3132@gmail.com>
2023-10-16 12:26:04 +0000

    deleted notebook, organize inputs of multidate workflow

--- examples/example_workflow.py
+++ examples/example_workflow.py
@@ -54,31 +54,35 @@ def simulation_dates_parser(date_list: list) -> list:
 
 def test_run(flexpart_code):
     """Run workflow."""
+    user_name='lfernand'
     simulation_dates = simulation_dates_parser(['2021-01-01, 2021-01-02'])
 
     # Links to the remote files/folders.
-    glc = orm.RemoteData(remote_path='/users/lfernand/resources/flexpart/GLC2000',
+    glc = orm.RemoteData(remote_path=f'/users/{user_name}/resources/flexpart/GLC2000',
                          computer=flexpart_code.computer)
     species = orm.RemoteData(
-        remote_path='/users/lfernand/resources/flexpart/SPECIES',
+        remote_path=f'/users/{user_name}/resources/flexpart/SPECIES',
         computer=flexpart_code.computer)
     surfdata = orm.RemoteData(
-        remote_path='/users/lfernand/resources/flexpart/surfdata.t',
+        remote_path=f'/users/{user_name}/resources/flexpart/surfdata.t',
         computer=flexpart_code.computer)
     surfdepo = orm.RemoteData(
-        remote_path='/users/lfernand/resources/flexpart/surfdepo.t',
+        remote_path=f'/users/{user_name}/resources/flexpart/surfdepo.t',
         computer=flexpart_code.computer)
     meteo_path = orm.RemoteData(
-        remote_path='/scratch/snx3000/lfernand/FLEXPART_input/cosmo7/',
+        remote_path=f'/scratch/snx3000/{user_name}/FLEXPART_input/cosmo7/',
         computer=flexpart_code.computer)
     
-
+    #builder starts
     workflow = plugins.WorkflowFactory('flexpart.multi_dates')
     builder = workflow.get_builder()
-    builder.code = flexpart_code
+    builder.fcosmo_code = flexpart_code
+    builder.check_meteo_cosmo_code =  orm.load_code('test_bash_2@daint-direct')
+    builder.simulation_dates = simulation_dates
+
     builder.model=orm.Str('cosmo7')
     builder.meteo_path = meteo_path
-    builder.gribdir=orm.Str('/scratch/snx3000/lfernand/FLEXPART_input/')
+    builder.gribdir=orm.Str(f'/scratch/snx3000/{user_name}/FLEXPART_input/')
     builder.outgrid = orm.Dict(
         dict=read_yaml_data('inputs/outgrid.yaml', names=[
             'Europe',
@@ -87,11 +91,16 @@ def test_run(flexpart_code):
         'inputs/outgrid_nest.yaml', names=[
             'Europe',
         ])['Europe'])  # optional input
-    builder.simulation_dates = simulation_dates
+   
+    #model settings
+    builder.command = orm.Dict(dict=read_yaml_data('inputs/command.yaml'))
+    builder.input_phy = orm.Dict(dict=read_yaml_data('inputs/input_phy.yaml'))
+    builder.release_settings = orm.Dict(dict=read_yaml_data('inputs/release.yaml'))
     builder.locations = orm.Dict(
         dict=read_yaml_data('inputs/locations.yaml', names=[
             'TEST_32',
         ]))
+    
     builder.meteo_inputs = orm.Dict(
         dict=read_yaml_data('inputs/meteo_inputs.yaml', names=[
             'cosmo7',
