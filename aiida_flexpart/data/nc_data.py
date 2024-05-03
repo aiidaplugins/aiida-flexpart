2e924fbfc5dc9d394d7d65df4f6c809345f57fdc
LucR31 <94859181+LucR31@users.noreply.github.com>
2024-05-03 09:07:52 +0000

    inprove class

--- aiida_flexpart/data/nc_data.py
+++ aiida_flexpart/data/nc_data.py
@@ -1,14 +1,12 @@
 import os
 from aiida.orm import RemoteData
 from netCDF4 import Dataset
-import tempfile
-from pathlib import Path
 
 class NetCDFData(RemoteData):
 
     def __init__(self, filepath=None, remote_path=None, **kwargs):
         """
-        Data plugin for Netcdf objects.
+        Data plugin for Netcdf files.
         """
         super().__init__(**kwargs)
         if filepath is not None:
@@ -16,34 +14,31 @@ class NetCDFData(RemoteData):
             self.set_remote_path(remote_path)
             self.set_filename(filename)
 
-            #open and read as NetCDF
-            nc_file = Dataset(filepath, mode='r')
+            # open and read as NetCDF
+            nc_file = Dataset(filepath, mode="r")
             self.set__global_attributes(nc_file)
 
-            #put object in repo
-            #self.put_object_from_file(filepath, filename)    
-
     def set_filename(self, val):
-        self.base.attributes.set('filename', val)
+        self.base.attributes.set("filename", val)
 
-    def set__global_attributes(self, nc_file):
+    def set_global_attributes(self, nc_file):
 
         g_att = {}
         for a in nc_file.ncattrs():
             g_att[a] = repr(nc_file.getncattr(a))
-        self.base.attributes.set('global_attributes', g_att) 
+        self.base.attributes.set("global_attributes", g_att)
 
-        nc_dimensions = {i:len(nc_file.dimensions[i]) for i in nc_file.dimensions}
-        self.base.attributes.set('dimensions', nc_dimensions)
+        nc_dimensions = {i: len(nc_file.dimensions[i]) for i in nc_file.dimensions}
+        self.base.attributes.set("dimensions", nc_dimensions)
 
     def ncdump(self):
         """
         Small python version of ncdump.
         """
         print("dimensions:")
-        for k,v in self.base.attributes.get('dimensions').items():
-            print('\t%s =' %k,v)
+        for k, v in self.base.attributes.get("dimensions").items():
+            print("\t%s =" % k, v)
 
-        print('// global attributes:')
-        for k,v in self.base.attributes.get('global_attributes').items():
-            print('\t:%s =' %k,v)
+        print("// global attributes:")
+        for k, v in self.base.attributes.get("global_attributes").items():
+            print("\t:%s =" % k, v)
