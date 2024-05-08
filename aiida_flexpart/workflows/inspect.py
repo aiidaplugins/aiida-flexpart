5c53c36a4c2a26ac813428db618c335a3fc08ee1
LucR31 <94859181+LucR31@users.noreply.github.com>
2024-05-08 08:15:44 +0000

    bugfixes

--- aiida_flexpart/workflows/inspect.py
+++ aiida_flexpart/workflows/inspect.py
@@ -23,6 +23,9 @@ def check(nc_file):
             if i[0] == nc_file.attributes['global_attributes']['created']:
                 return False
     return True
+
+def validate_version(nc_file):
+    return 'created' in nc_file.attributes['global_attributes'].keys()
     
 @calcfunction
 def store(i,folder):
@@ -32,6 +35,8 @@ def store(i,folder):
                         remote_path = str(Path(folder.get_remote_path())),
                         computer = folder.computer
                      )
+        if validate_version(node):
+            return node
         if check(node):
             return node
         return
@@ -41,15 +46,26 @@ class InspectWorkflow(WorkChain):
     def define(cls, spec):
         super().define(spec)
         spec.input_namespace('remotes',valid_type=orm.RemoteData,required=False)
+        spec.input_namespace('remotes_cs',valid_type=orm.RemoteStashFolderData,required=False)
         spec.outputs.dynamic = True
         spec.outline(
+            cls.fill_remote_data,
             cls.inspect,
             cls.results,
         )
 
+    def fill_remote_data(self):
+        self.ctx.dict_remote_data = {}
+        if 'remotes' in self.inputs:
+            self.ctx.dict_remote_data = self.inputs.remotes
+        else:
+            for k,v in self.inputs.remotes_cs.items():
+                self.ctx.dict_remote_data[k] = orm.RemoteData(remote_path = v.target_basepath,
+                                                computer = v.computer
+                                                )
     def inspect(self):
         self.ctx.list_files = []
-        for v in self.inputs.remotes.values():
+        for v in self.ctx.dict_remote_data.values():
                 for i in v.listdir():
                     if '.nc' in i:
                         self.ctx.list_files.append(i)
