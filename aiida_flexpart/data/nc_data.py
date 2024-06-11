ab50ad79693e4cb9f10107e8c1d02064f09c84d8
LucR31 <94859181+LucR31@users.noreply.github.com>
2024-06-11 12:51:46 +0000

    inspect workflow and small additions in nc_data

--- aiida_flexpart/data/nc_data.py
+++ aiida_flexpart/data/nc_data.py
@@ -1,10 +1,12 @@
 import os
 from aiida.orm import RemoteData
-from netCDF4 import Dataset
 
-class NetCDFData(RemoteData):
 
-    def __init__(self, filepath=None, remote_path=None, **kwargs):
+class NetCdfData(RemoteData):
+
+    def __init__(
+        self, filepath=None, remote_path=None, g_att=None, nc_dimensions=None, **kwargs
+    ):
         """
         Data plugin for Netcdf files.
         """
@@ -13,32 +15,20 @@ class NetCDFData(RemoteData):
             filename = os.path.basename(filepath)
             self.set_remote_path(remote_path)
             self.set_filename(filename)
-
-            # open and read as NetCDF
-            nc_file = Dataset(filepath, mode="r")
-            self.set_global_attributes(nc_file)
+            self.set_global_attributes(g_att, nc_dimensions)
 
     def set_filename(self, val):
         self.base.attributes.set("filename", val)
 
-    def set_global_attributes(self, nc_file):
-
-        g_att = {}
-        for a in nc_file.ncattrs():
-            g_att[a] = repr(nc_file.getncattr(a))
+    def set_global_attributes(self, g_att, nc_dimensions):
         self.base.attributes.set("global_attributes", g_att)
-
-        nc_dimensions = {i: len(nc_file.dimensions[i]) for i in nc_file.dimensions}
         self.base.attributes.set("dimensions", nc_dimensions)
 
     def ncdump(self):
-        """
-        Small python version of ncdump.
-        """
+        """Small python version of ncdump."""
         print("dimensions:")
         for k, v in self.base.attributes.get("dimensions").items():
-            print("\t%s =" % k, v)
-
+            print(f"\t {k} = {v}")
         print("// global attributes:")
         for k, v in self.base.attributes.get("global_attributes").items():
-            print("\t:%s =" % k, v)
+            print(f"\t :{k} = {v}")
